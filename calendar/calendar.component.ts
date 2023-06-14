49535cfd262451c504298fd27ab65300a6e86d4e
holywaterBetter <holywater.better@gmail.com>
2023-06-14 23:10:01 +0900

    feat(#1): 캘린더 초기 구현

--- calendar/calendar.component.ts
+++ calendar/calendar.component.ts
@@ -6,7 +6,73 @@ import { Component } from "@angular/core";
   styleUrls: ["./calendar.component.scss"],
 })
 export class CalendarComponent {
+  date = new Date();
+  currentYear: number = this.date.getFullYear();
+  currentMonth: number = this.date.getMonth() + 1;
+  calendar: number[][] = this.getCalendar(this.currentYear, this.currentMonth);
+
   constructor() {}
+
+  isPast(date: number): boolean {
+    return date < this.date.getDate();
+  }
+
+  getCalendar(year: number, month: number): number[][] {
+    const calendar: number[][] = [];
+
+    const daysInMonth = new Date(year, month, 0).getDate();
+    const firstDayOfWeek = new Date(year, month - 1, 1).getDay();
+
+    let currentDate = 1;
+    for (let i = 0; i < 6; i++) {
+      const week: number[] = [];
+
+      if (i === 0) {
+        for (let j = 0; j < firstDayOfWeek; j++) {
+          week.push(0);
+        }
+      }
+
+      for (let j = week.length; j < 7; j++) {
+        if (currentDate <= daysInMonth) {
+          week.push(currentDate);
+          currentDate++;
+        } else {
+          week.push(0);
+        }
+      }
+
+      calendar.push(week);
+
+      if (currentDate > daysInMonth) {
+        break;
+      }
+    }
+
+    return calendar;
+  }
+
+  previousMonth() {
+    if (this.currentMonth === 1) {
+      this.currentMonth = 12;
+      this.currentYear--;
+    } else {
+      this.currentMonth--;
+    }
+
+    this.calendar = this.getCalendar(this.currentYear, this.currentMonth);
+  }
+
+  nextMonth() {
+    if (this.currentMonth === 12) {
+      this.currentMonth = 1;
+      this.currentYear++;
+    } else {
+      this.currentMonth++;
+    }
+
+    this.calendar = this.getCalendar(this.currentYear, this.currentMonth);
+  }
 }
 
 const MonthlyData = [
