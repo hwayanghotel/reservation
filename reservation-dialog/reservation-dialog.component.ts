d6f1ec9bfd54ae06020af9b6104169be2120b949
holywaterBetter <holywater.better@gmail.com>
2023-06-27 10:08:59 +0900

    feat(#11): 예약폼 수정 - 예약 dialog

---
+++ reservation-dialog/reservation-dialog.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { ReservationService } from "reservation/service/reservation.service";
+
+@Component({
+    selector: "reservation-dialog",
+    templateUrl: "./reservation-dialog.component.html",
+    styleUrls: ["./reservation-dialog.component.scss"],
+})
+export class ReservationDialogComponent {
+    step: number;
+    constructor(private reservationService: ReservationService) {
+        this.reservationService.bookingStep$.subscribe((step) => {
+            console.warn("ReservationDialogComponent step", step);
+            this.step = step;
+        });
+    }
+}
