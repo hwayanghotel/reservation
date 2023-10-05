b343d0d3ace1e8b540ab4aaef59109a9a9586a76
holywaterBetter <holywater.better@gmail.com>
2023-10-05 12:30:38 +0900

    feat(#18): [예약] 예약자 인원 선택창 신규 구현

---
+++ booking/booking-number-guest/booking-number-guest.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+export interface NumberOfGuests {
+    person: number;
+    kids: number;
+}
+
+@Component({
+    selector: "booking-number-guest",
+    templateUrl: "./booking-number-guest.component.html",
+    styleUrls: ["./booking-number-guest.component.scss"],
+})
+export class BookingNumberGuestComponent {
+    @Input("numberOfGuests") guest: NumberOfGuests = { person: 4, kids: 0 };
+    @Output() completeNumberOfGuests = new EventEmitter<NumberOfGuests>();
+    constructor() {}
+
+    onBackButton() {
+        window.history.back();
+    }
+
+    onNextButton() {
+        this.completeNumberOfGuests.emit(this.guest);
+    }
+}
