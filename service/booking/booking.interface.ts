d5d484c96e96ea75f56558f9f499f37c9b925e1c
holywaterBetter <holywater.better@gmail.com>
2023-11-13 04:09:09 +0900

    feat(#18) : 가격 등 상수 재정의

--- service/booking/booking.interface.ts
+++ service/booking/booking.interface.ts
@@ -9,3 +9,41 @@ export interface IBookingService {
 }
 
 export const BOOKING_COLLECTION = "BOOKING";
+
+export const MAX_BOOKING = {
+    foodTable: 16,
+    flatTable: 6,
+    dechTable: 6,
+    parking: 40,
+};
+
+export const STANDARD_BOOKING = {
+    foodTableGuests: {
+        std: 4,
+        max: 5,
+    },
+    flatTableGuests: {
+        std: 4,
+        max: 7,
+    },
+    dechTableGuests: {
+        std: 4,
+        max: 7,
+    },
+};
+
+export const Price = {
+    평상: 50000,
+    데크: 40000,
+    평상추가인원: 10000,
+    능이백숙: 90000,
+    백숙: 70000,
+    버섯찌개: 60000,
+    버섯찌개2: 40000,
+    더덕구이: 30000,
+    파전: 20000,
+    묵: 15000,
+    동동주: 10000,
+    막걸리: 5000,
+    주류: 5000,
+};
