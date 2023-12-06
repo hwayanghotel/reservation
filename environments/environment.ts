c6d419505f9ace7e21adf228a8b81d010a05465f
holywaterBetter <holywater.better@gmail.com>
2023-12-07 01:02:12 +0900

    Firebase 환경 이동

---
+++ environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+        projectId: 'hwayanghotel-a7178',
+        appId: '1:530867247169:web:bd540c747c5d2135b80ec0',
+        storageBucket: 'hwayanghotel-a7178.appspot.com',
+        locationId: 'asia-northeast3',
+        apiKey: 'AIzaSyA3A6kldyOfDOhYVg633eXik1CjbtXxbeE',
+        authDomain: 'hwayanghotel-a7178.firebaseapp.com',
+        messagingSenderId: '530867247169',
+        measurementId: 'G-Y9MHN92W15',
+    },
+};
